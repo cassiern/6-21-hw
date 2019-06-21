c307a206580eb090fa0c38668a996d68ce89552e
Cassie Nichols <cassienichols@macbooks-MacBook-Pro.local>
2019-06-21 16:25:10 -0600

    The Farm - conditionals

--- js/app.js
+++ js/app.js
@@ -36,24 +36,31 @@
 // console.log(`Hello my name is ${myName}`);
 
 //C. BOOLEANS
-  const a = 4;
-  const b = 53;
-  const c = 57;
-  const d = 16;
-  const e = 'Kevin';
+  // const a = 4;
+  // const b = 53;
+  // const c = 57;
+  // const d = 16;
+  // const e = 'Kevin';
 
-  console.log(a < b);
-  console.log(c > d);
-  console.log('Name' === 'Name');
-  // FOR THE NEXT TWO, USE ONLY && OR ||
-  console.log(true || false);
-  console.log(false && false && false && false && false || true);
-  console.log(false === false)
-  console.log(e == 'Kevin');
-  console.log(a !== b || c); // note: a < b < c is NOT CORRECT (and is not a valid JS expression)
-  console.log(a === a || d); // note: the answer is a simple arithmetic equation, not something "weird"
-  console.log(48 == '48');
+  // console.log(a < b);
+  // console.log(c > d);
+  // console.log('Name' === 'Name');
+  // // FOR THE NEXT TWO, USE ONLY && OR ||
+  // console.log(true || false);
+  // console.log(false && false && false && false && false || true);
+  // console.log(false === false)
+  // console.log(e == 'Kevin');
+  // console.log(a !== b || c); // note: a < b < c is NOT CORRECT (and is not a valid JS expression)
+  // console.log(a === a || d); // note: the answer is a simple arithmetic equation, not something "weird"
+  // console.log(48 == '48');
 
+//D. THE FARM
+const animal = "chicken";
+if (animal === "cow"){
+	console.log("Mooooo");
+} else {
+	console.log("Hey! You're not a cow!")
+}
 
 
 
