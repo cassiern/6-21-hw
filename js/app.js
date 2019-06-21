69a1d45dcc66068abca30978538cab79245e5b2f
Cassie Nichols <cassienichols@macbooks-MacBook-Pro.local>
2019-06-21 15:57:55 -0600

    Q+A

---
+++ js/app.js
@@ -0,0 +1,23 @@
+//1. VARIABLES & DATATYPES 	
+//A. Q+A
+	// 1. We assign a value to a variable using const and let.
+	// What is on the right of the "=" is the value assigned
+	// to what's on the left of the "=".
+	// 2. We change the value of a variable using
+	// let. Then we declare whatever variable we want to change,
+	// equal to what we want to change it to.
+	// It looks like this let Variable = 1; Variable = 2; 
+	// 3. We assign an existing variable to a new variable
+	// by putting the new variable on the let side of the
+	// "=" and the old variable. 
+	// 4. Declaring a variable is the 'let' or 'const'. You're
+	// declaring a variable. The assigning is th '='.
+	// And the defining is what you put on the right side
+	// of the '='. That is seting the value to the variable.
+	// 5. Pseudocoding is writing out what exact steps you
+	// are wanting to take in order to do your code. 
+	// Pseudocode is writen in plain engligh, not in code. 
+	// And for that reason pseudocode is a great way to break
+	// down complicated code.
+	// Probably 70% of your time should be committed to
+	// planning and solving your code.  
